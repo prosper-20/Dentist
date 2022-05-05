f0f726852683e85d0ed57135c2f015186ba455f7
prosper-20 <edwardprosper001@gmail.com>
2022-05-05 13:57:36 +0100

    updates

--- app/models.py
+++ app/models.py
@@ -1,3 +1,15 @@
+from distutils.command.upload import upload
+from tabnanny import verbose
 from django.db import models
 
 # Create your models here.
+
+class Services(models.Model):
+    name = models.CharField(max_length=100)
+    image = models.ImageField(upload_to="service_images")
+
+    class Meta:
+        verbose_name_plural = "Services"
+
+    def __str__(self):
+        return self.name
