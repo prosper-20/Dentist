9314b7d85479b9010f7d343dcd020aa39054f44d
prosper-20 <edwardprosper001@gmail.com>
2022-05-05 11:15:46 +0100

    updates

--- PROJECT/settings.py
+++ PROJECT/settings.py
@@ -37,7 +37,8 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-]
+    'app.apps.AppConfig',
+    ]
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
